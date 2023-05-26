9d235c6109023ccc4a44177e571a6b4daf5e3785
dbdPwls <dbddpwls@naver.com>
2023-05-27 03:23:31 +0900

    버스지도 및 노선, 지도 화면, 회원가입 화면 추가

---
+++ map/map.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>어쩌다 제주</title>
+</head>
+<link rel="stylesheet" type="text/css" href="css\style.css">
+    <body style="background-color:#FFA500; overflow: hidden;">
+        <div style="display:flex;">
+            <p><img src="images\title_logo1.png" class="title_logo"></p>
+        </div>
+        <div class="home_page">
+            <div class="map_main">
+                <!--지도 화면-->
+            </div>
+        </div>
+        <div class="footer">
+            <ul>
+                <li class="tap_menu"><a href="index.php"><img src="images\Home.png"></a></li>
+                <li class="tap_menu"><a href=""><img src="images\Explore2.png"></a></li>
+                <li class="tap_menu"><a href=""><img src="images\Frame.png"></a></li>
+                <li class="tap_menu"><a href=""><img src="images\Icon.png"></a></li>
+            </ul>
+        </div>
+    </body>
+</html>
