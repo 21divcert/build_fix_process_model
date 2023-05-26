4752f9ae8cc2092670140faa7ea95c89a7e1bfaf
8_Jin <103305387+dbdPwls@users.noreply.github.com>
2023-05-27 04:15:14 +0900

    Update map.php

--- map/map.php
+++ map/map.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<title>어쩌다 제주</title>
 </head>
-<link rel="stylesheet" type="text/css" href="css\style.css">
+<link rel="stylesheet" type="text/css" href="..\css\style.css">
     <body style="background-color:#FFA500; overflow: hidden;">
         <div style="display:flex;">
-            <p><img src="images\title_logo1.png" class="title_logo"></p>
+            <p><img src="..\images\title_logo1.png" class="title_logo"></p>
         </div>
         <div class="home_page">
             <div class="map_main">
@@ -16,10 +16,10 @@
         </div>
         <div class="footer">
             <ul>
-                <li class="tap_menu"><a href="index.php"><img src="images\Home.png"></a></li>
-                <li class="tap_menu"><a href=""><img src="images\Explore2.png"></a></li>
-                <li class="tap_menu"><a href=""><img src="images\Frame.png"></a></li>
-                <li class="tap_menu"><a href=""><img src="images\Icon.png"></a></li>
+                <li class="tap_menu"><a href="..\index.php"><img src="..\images\Home.png"></a></li>
+                <li class="tap_menu"><a href=""><img src="..\images\Explore2.png"></a></li>
+                <li class="tap_menu"><a href=""><img src="..\images\Frame.png"></a></li>
+                <li class="tap_menu"><a href=""><img src="..\images\Icon.png"></a></li>
             </ul>
         </div>
     </body>
