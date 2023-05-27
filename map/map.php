db5e9daddf4e8a8156f84c87e0e54e5eb9a49e59
nam-kiyun <fordnam1@gmail.com>
2023-05-27 16:07:05 +0900

    Revert "멥 페이지 수정"
    This reverts commit 370410aa4859e0811853f85570d141f1e94c77ef.

--- map/map.php
+++ map/map.php
@@ -12,17 +12,6 @@
         <div class="home_page">
             <div class="map_main">
                 <!--지도 화면-->
-                <div id="map" style="width:500px;height:400px;"></div>
-                <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9be8de46cc892ba13e3d93b26b3600ad"></script>
-                <script>
-                    var container = document.getElementById('map');
-                    var option = {
-                        center : new kakao.maps.LatLng(33.450701, 126.570667),
-                        level: 3
-                    };
-
-                    var map = new kakao.maps.Map(container, options);
-                </script>
             </div>
         </div>
         <div class="footer">
