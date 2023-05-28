8b1583a1edef988ff568bd4984c8c07ad8c9867d
dbdPwls <dbddpwls@naver.com>
2023-05-29 02:28:31 +0900

    버스 정류장 추가

--- index.php
+++ index.php
@@ -49,6 +49,68 @@
                         <li class="bus_up">n분 전</li> 
                         <li><strong>송당리 마을</strong></li>
                         <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>다랑쉬오름 입구(남), 손지오름</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>용눈이오름</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>제주레일바이크</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>다랑쉬오름 입구(북)</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>비자림</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>메이즈랜드</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>둔지오름</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>덕천리</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>어대오름</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>한울랜드</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>송당리 마을</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>국가위성운영센터</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>동백동산 습지센터</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>알밤오름</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>다희연</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>선인동</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>선녀와 나무꾼</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>선흘2리/양잠단지</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>제주세계자연유산센터입구</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">n분 전</li> 
+                        <li><strong>제주세계자연유산센터입구 거문오름</strong></li>
+                        <li class="bus_down">n분 전</li> <br>
+                        <li class="bus_up">출발지 & 종점</li> 
+                        <li><strong>대천환승정류장</strong></li>
                     </ul>
                 </div>
             </div>
@@ -89,6 +151,70 @@
                             <li class="bus_up">n분 전</li> 
                             <li><strong>소인국테마파크</strong></li>
                             <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>서광서리</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>노리매</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>구억리</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>신평리</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>인향동</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>산양 큰엉곶 입구</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주평화박물관</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>청수마을회관</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>청수리사거리</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>저지리사무소</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주현대미술관, 김창열미술관</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>방림원</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>생각하는정원</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>환상숲곶자왈공원</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주유리의성</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주오설록 티뮤지엄</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주항공우주호텔</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주항공우주박물관</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>제주신화월드 소방서</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>신화월드리조트</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li class="bus_up">n분 전</li> 
+                            <li><strong>삼밭구석 입구</strong></li>
+                            <li class="bus_down">n분 전</li> <br>
+                            <li style="float: right;">출발지 & 종점</li><li><strong>동광환승정류장</strong></li>
                         </ul>
                     </div>
                 </div>
