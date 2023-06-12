1d5905cf10dea05eb3c4ed9c9295d44b0a3d431c
KANG MINJEONG <rkddkwl3414@naver.com>
2023-06-13 01:34:02 +0900

    tag 페이지 팝업창 조금 수정

--- tag.php
+++ tag.php
@@ -25,6 +25,9 @@
         <div class="main">
             <div class="tour_info" style="display: none;">
                 <div class="tour_wrap">
+                    <div class="tour_close_btn">
+                        <p>닫기</p>
+                    </div>
                     <div class="tour_title">
                         <p class="tour_name">제주어울림감귤체험농장</p>
                     </div>
@@ -33,12 +36,15 @@
                     </div>
                     <div class="tour_info_box">
                         <!-- <p class="tour_name">제주어울림감귤체험농장</p> -->
-                        <p class="tour_loc">제주특별자치도 서귀포시 성산읍 수산리 1948</p>
+                        <div class = "tour_location_box">
+                            <p><img src="./images/location.png"></p>
+                            <p class="tour_loc">제주특별자치도 서귀포시 성산읍 수산리 1948</p>
+                            </div>
+                        <p class = "tour_tagList">Information</p>
+                        
                         <p class="tour_info_text">5월 5일까지 연장된 '사회적 거리두기'로 가고 싶은 곳, 하고 싶은 것들이 점점 늘어간다. 오랫동안 보지 못한 친구도 만나고 싶고, 답답한 실내에서 벗어나 탁 트인 자연을 느끼고픈 마음. 이 모든 걸 아우르는 게 결국 '여행' 아닐까? 한 설문에 따르면 코로나19가 종식되면 가장 하고 싶은 일 1위가 국내여행이고, 여행지에서 가고 싶은 곳 1위는 바다, 산과 같은 자연, 2위는 공원, 수목원, 그 뒤로 낚시, 캠핑과 같은 레저 활동이라는 답변이 이어졌다. 마음껏 밖을 나서도 될 그날이 오면 언제든 떠날 수 있도록, 랜선으로 미리 보며 여행을 계획해보면 어떨까? 영상이 플레이되는 동안 마음은 이미 제주에 다다를지도 모른다. </p>
                     </div>
-                    <div class="tour_close_btn">
-                        <p>닫기</p>
-                    </div>
+
                 </div>
             </div>
             <div class = "main_wrap" style="width:90%!important">
